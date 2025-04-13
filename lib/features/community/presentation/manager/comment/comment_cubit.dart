80156d8a2110ec59b140224b949df90dd3fd2bd9
Amira Shawki <amirashawki7@gmail.com>
2025-04-13 20:56:53 +0000

    Comment Cubit and state done

--- lib/features/community/presentation/manager/comment/comment_cubit.dart
+++ lib/features/community/presentation/manager/comment/comment_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:evolvify/features/community/data/models/comment_model.dart';
+import 'package:evolvify/features/community/data/repo/repo_post_impl.dart';
 import 'package:meta/meta.dart';
 
 part 'comment_state.dart';
 
 class CommentCubit extends Cubit<CommentState> {
   CommentCubit() : super(CommentInitial());
+  emit(CommentLoading);
+  
+  Future<void> like(postId,content) async {
+    var result = await RepoPostImpl().commentOnPost(postId, content);
+    result.fold(
+      (failure) {
+        emit(CommentFailure(failure.errMessge));
+      },
+      (comment) {
+        emit(CommentSuccess(comment));
+      },
+    );
+  }
 }
--- lib/features/community/presentation/manager/comment/comment_state.dart
+++ lib/features/community/presentation/manager/comment/comment_state.dart
@@ -4,3 +4,12 @@ part of 'comment_cubit.dart';
 sealed class CommentState {}
 
 final class CommentInitial extends CommentState {}
+class CommentLoading extends CommentState {}
+class CommentSuccess extends CommentState {
+  final CommentModel comment;
+  CommentSuccess(this.comment);
+}
+class CommentFailure extends CommentState {
+  final String error;
+  CommentFailure(this.error);
+}
