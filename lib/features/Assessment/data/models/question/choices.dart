c3263e9149591965e21199e0f40b5fa14abde5e8
Amira Shawki <amirashawki7@gmail.com>
2025-04-27 15:24:36 +0000

    question_model done

--- lib/features/Assessment/data/models/question/choices.dart
+++ lib/features/Assessment/data/models/question/choices.dart
@@ -1,36 +1,36 @@
-// import 'dart:convert';
+import 'dart:convert';
 
-// import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
 
-// class Choices extends Equatable {
-//   final String? a;
-//   final String? b;
-//   final String? c;
-//   final String? d;
+class Choices extends Equatable {
+  final String? a;
+  final String? b;
+  final String? c;
+  final String? d;
 
-//   const Choices({this.a, this.b, this.c, this.d});
+  const Choices({this.a, this.b, this.c, this.d});
 
-//   factory Choices.fromMap(Map<String, dynamic> data) => Choices(
-//     a: data['a'] as String?,
-//     b: data['b'] as String?,
-//     c: data['c'] as String?,
-//     d: data['d'] as String?,
-//   );
+  factory Choices.fromMap(Map<String, dynamic> data) => Choices(
+    a: data['a'] as String?,
+    b: data['b'] as String?,
+    c: data['c'] as String?,
+    d: data['d'] as String?,
+  );
 
-//   Map<String, dynamic> toMap() => {'a': a, 'b': b, 'c': c, 'd': d};
+  Map<String, dynamic> toMap() => {'a': a, 'b': b, 'c': c, 'd': d};
 
-//   /// `dart:convert`
-//   ///
-//   /// Parses the string and returns the resulting Json object as [Choices].
-//   factory Choices.fromJson(String data) {
-//     return Choices.fromMap(json.decode(data) as Map<String, dynamic>);
-//   }
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Choices].
+  factory Choices.fromJson(String data) {
+    return Choices.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
 
-//   /// `dart:convert`
-//   ///
-//   /// Converts [Choices] to a JSON string.
-//   String toJson() => json.encode(toMap());
+  /// `dart:convert`
+  ///
+  /// Converts [Choices] to a JSON string.
+  String toJson() => json.encode(toMap());
 
-//   @override
-//   List<Object?> get props => [a, b, c, d];
-// }
+  @override
+  List<Object?> get props => [a, b, c, d];
+}
