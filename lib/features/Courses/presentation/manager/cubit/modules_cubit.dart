ccb7df7748563720e91ac15565c18c951da768cd
Amira Shawki <amirashawki7@gmail.com>
2025-05-08 18:49:11 +0000

    modules Of Course  cubit done

--- lib/features/Courses/presentation/manager/cubit/modules_cubit.dart
+++ lib/features/Courses/presentation/manager/cubit/modules_cubit.dart
@@ -3,21 +3,20 @@ import 'package:equatable/equatable.dart';
 import 'package:evolvify/features/Courses/data/models/modules/modules_of_course.dart';
 import 'package:evolvify/features/Courses/data/repo/courses_repo_impl.dart';
 
-
 part 'modules_state.dart';
 
 class ModulesCubit extends Cubit<ModulesState> {
   ModulesCubit() : super(ModulesInitial());
-    Future<void> getModulesOfCourse({required id}) async {
+  Future<void> getModulesOfCourse({required id}) async {
     emit(ModulesLosding());
-    var modulesOfCourse = await CoursesRepoImpl().getModulesOfCourse(id:id );
+    var modulesOfCourse = await CoursesRepoImpl().getModulesOfCourse(id: id);
     modulesOfCourse.fold(
       (failure) {
-        emit(ModulesFailure(errMessage:failure.errMessge));
+        emit(ModulesFailure(errMessage: failure.errMessge));
       },
       (modulesOfCourseList) {
         emit(ModulesSuccess(modulesOfCourseList: modulesOfCourseList));
-       
+        print(modulesOfCourseList);
       },
     );
   }
