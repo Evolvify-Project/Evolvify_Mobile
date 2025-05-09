3a1198efa76a29ac783195e4f3b627955aaa062c
Amira Shawki <amirashawki7@gmail.com>
2025-05-09 18:47:35 +0000

    Aiassessment View done

--- lib/features/AI-Assessment/presentation/views/AI-Assessment_view.dart
+++ lib/features/AI-Assessment/presentation/views/AI-Assessment_view.dart
@@ -1,4 +1,6 @@
-
+import 'package:evolvify/core/utils/app_style.dart';
+import 'package:evolvify/core/utils/constant.dart';
+import 'package:evolvify/core/widgets/custom_button.dart';
 import 'package:evolvify/features/AI-Assessment/presentation/views/widgets/PremiumSection.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +13,39 @@ class AiassessmentView extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
-          SizedBox(height: 30),
+          SizedBox(height: 35),
           UpgradetoPremiumSection(),
+          SizedBox(height: 47),
+          SizedBox(height: 18),
+          Text(
+            'Try Now',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: AppColors.kPrimaryColor,
+              fontSize: getResponsiveFontSize(context, fontSize: 26),
+            ),
+          ),
+          SizedBox(height: 18),
+          Text(
+            'Enhance your soft skills through immersive \n                                     simulations.',
+            style: TextStyle(
+              color: Color(0xff92919A),
+              fontWeight: FontWeight.w400,
+              fontSize: getResponsiveFontSize(context, fontSize: 16),
+            ),
+          ),
+          SizedBox(height: 43),
+          CustomButton(
+            title: 'Start Presentation Analysis',
+            borderRadius: 15,
+            onTap: () {},
+          ),
+          SizedBox(height: 17),
+          CustomButton(
+            title: 'Start Interview Analysis',
+            borderRadius: 15,
+            onTap: () {},
+          ),
         ],
       ),
     );
