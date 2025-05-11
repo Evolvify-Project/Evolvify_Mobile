f74dea7151fab387ed7350bf0735205417d39c1e
Amira Shawki <amirashawki7@gmail.com>
2025-05-11 14:36:25 +0000

    Search Repo Impl done

--- lib/features/search/data/repo/search_repo.dart
+++ lib/features/search/data/repo/search_repo.dart
@@ -4,6 +4,12 @@ import 'package:evolvify/features/Assessment/data/models/courses_model.dart';
 
 abstract class SearchRepo {
 
-  Future<Either<Failure, List<CoursesModel>>> search(String query);
+  Future<Either<Failure, List<CoursesModel>>> search(String query,{
+    int pageNumber,
+    int pageSize,
+    int sortBy,
+     int? skillId,
+  int? level,
+  });
 
 }
---
+++ lib/features/search/data/repo/search_repo_impl.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:evolvify/core/errors/failures.dart';
+import 'package:evolvify/core/utils/api_services.dart';
+import 'package:evolvify/features/Assessment/data/models/courses_model.dart';
+import 'package:evolvify/features/search/data/repo/search_repo.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  @override
+  Future<Either<Failure, List<CoursesModel>>> search(
+    String query, {
+    int pageNumber = 1,
+    int pageSize = 10,
+    int sortBy = 1,
+    int? skillId,
+    int? level,
+  }) async {
+    try {
+      var data = await ApiServices().get(
+        endPoint:
+           'Courses?Search=$query&PageNumber=$pageNumber'
+      '&PageSize=$pageSize&SortBy=$sortBy'
+      '${skillId != null ? '&SkillId=$skillId' : ''}'
+      '${level != null ? '&Level=$level' : ''}'
+      );
+      List<CoursesModel> searchCoursesList =
+          (data["data"] as List)
+              .map((course) => CoursesModel.fromJson(course))
+              .toList();
+      return right(searchCoursesList);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
