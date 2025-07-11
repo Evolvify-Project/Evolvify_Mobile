1b526508b76c4eeedc04104a98f88428ed65a7a7
AhmadMoemen <ahmadxmoemendjz11@gmail.com>
2025-07-11 03:01:35 +0300

    Revert isEmulator boolean

--- android/app/src/main/kotlin/com/example/evolvify/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/evolvify/MainActivity.kt
@@ -24,19 +24,4 @@ class MainActivity: FlutterActivity() {
         }
     }
 
-    private fun isEmulator(): Boolean {
-        return (Build.FINGERPRINT.startsWith("generic")
-                || Build.FINGERPRINT.startsWith("unknown")
-                || Build.MODEL.contains("google_sdk")
-                || Build.MODEL.contains("Emulator")
-                || Build.MODEL.contains("Android SDK built for x86")
-                || Build.MANUFACTURER.contains("Genymotion")
-                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-                || "google_sdk" == Build.PRODUCT
-                || "sdk" == Build.PRODUCT
-                || "sdk_gphone64_arm64" == Build.PRODUCT
-                || "vbox86p" == Build.PRODUCT
-                || "emulator" == Build.PRODUCT
-                || "simulator" == Build.PRODUCT)
-    }
-}
+class MainActivity: FlutterActivity()
